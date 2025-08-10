5eaf08d5ddaa0b09434de85c2ab86e72ce9da9ab
jenosize <jame@jenosize.com>
2025-08-10 17:41:08 +0700

    feat: 🎸 first commit

---
+++ src/products/product.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductController } from './adapters/inbounds/product.controller';
+import { ProductMongoRepository } from './adapters/outbounds/product.mongo.repository';
+import { ProductSchema, productsCollectionName } from './adapters/outbounds/product.schema';
+import { productRepositoryToken } from './applications/ports/product.repository';
+import { CreateProductUseCase } from './applications/usecases/createProduct.usecase';
+import { DeleteProductByIdUseCase } from './applications/usecases/deleteProductById.usecase';
+import { GetAllProductsUseCase } from './applications/usecases/getAllProducts.usecase';
+import { GetProductByIdUseCase } from './applications/usecases/getProductById.usecase';
+import { UpdateProductByIdUseCase } from './applications/usecases/updateProductById.usecase';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: productsCollectionName, schema: ProductSchema }])],
+  controllers: [ProductController],
+  providers: [
+    {
+      provide: productRepositoryToken,
+      useClass: ProductMongoRepository,
+    },
+    CreateProductUseCase,
+    DeleteProductByIdUseCase,
+    GetAllProductsUseCase,
+    GetProductByIdUseCase,
+    UpdateProductByIdUseCase,
+  ],
+})
+export class ProductModule {}
